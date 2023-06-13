b5c51a9747f255ccd0b0281e954899d5b4a3ecb0
Alec Barker <avbbarker@icloud.com>
2023-06-12 21:04:29 -0400

    functions lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(customerStreet) {
+    if (customerStreet > 42) 
+        return customerStreet - 42
+    else
+        return 42 - customerStreet
+}
+
+function distanceFromHqInFeet(customerStreet) {
+    return distanceFromHqInBlocks(customerStreet) * 264;
+}
+function distanceTravelledInFeet(start, end) {
+    if (start > end)
+        return (start - end) * 264
+    else
+        return (end - start) * 264
+}
+function calculatesFarePrice (start, end) {
+    const distance = distanceTravelledInFeet(start, end);
+    
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return .02 * (distance - 400);
+    } else if (distance > 2000 && distance <=2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+}
